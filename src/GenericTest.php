32a460925a9910db7760a08238b1a195b972ed76
mutian <bhfqlz@qq.com>
2023-11-05 20:31:53 +0800

    notices:
    version 1.0.5

--- src/GenericTest.php
+++ src/GenericTest.php
@@ -11,11 +11,17 @@ abstract class GenericTest implements PUnitTest
     public static function run(string $class):void{
         $proxy = new Proxy();
         try {
+
             $reflectionClass = new ReflectionClass($class);
+
             $reflectionMethod = $reflectionClass->getMethod("Test");
+
             $instance = $reflectionClass->newInstance();
+
             $proxyInstance = $proxy->getProxyInstance($instance);
+
             $reflectionMethod->invoke($proxyInstance);
+
         }catch (ReflectionException $exception){
             echo "ERROR:\r\n";
             echo "\r\n";
